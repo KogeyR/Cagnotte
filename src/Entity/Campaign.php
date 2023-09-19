3910c5829431cc771d1412b0f38503aa5fb64f79
KogeyR <134587760+KogeyR@users.noreply.github.com>
2023-09-19 16:51:41 +0200

    campaign entity

--- src/Entity/Campaign.php
+++ src/Entity/Campaign.php
@@ -2,66 +2,117 @@
 
 namespace App\Entity;
 
+use App\Repository\CampaignRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * Campaign
- *
- * @ORM\Table(name="campaign")
- * @ORM\Entity
- */
+#[ORM\Entity(repositoryClass: CampaignRepository::class)]
 class Campaign
 {
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="id", type="string", length=32, nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?string $id = null;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="title", type="string", length=150, nullable=true)
-     */
-    private $title;
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $title = null;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="content", type="text", length=65535, nullable=true)
-     */
-    private $content;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $content = null;
 
-    /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(name="created_at", type="datetime", nullable=true)
-     */
-    private $createdAt;
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $create_at = null;
 
-    /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
-     */
-    private $updatedAt;
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $update_at = null;
 
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="goal", type="integer", nullable=true)
-     */
-    private $goal;
+    #[ORM\Column(nullable: true)]
+    private ?int $goal = null;
 
-    /**
-     * @var string|null
-     *
-     * @ORM\Column(name="name", type="string", length=150, nullable=true)
-     */
-    private $name;
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $name = null;
 
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
 
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->create_at;
+    }
+
+    public function setCreateAt(?\DateTimeImmutable $create_at): static
+    {
+        $this->create_at = $create_at;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->update_at;
+    }
+
+    public function setUpdateAt(?\DateTimeImmutable $update_at): static
+    {
+        $this->update_at = $update_at;
+
+        return $this;
+    }
+
+    public function getGoal(): ?int
+    {
+        return $this->goal;
+    }
+
+    public function setGoal(?int $goal): static
+    {
+        $this->goal = $goal;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+   
+    public function setId(?string $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
